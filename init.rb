55c7c45b46d53b011b2e61b5b7577a21948e8c8d
Ralph Gutkowski <rgtk@protonmail.com>
2017-12-22 20:32:40 +0100

    Version 1.0.0

--- init.rb
+++ init.rb
@@ -4,9 +4,11 @@ Redmine::Plugin.register :redmine_impersonate do
   name "Redmine Impersonate"
   author "Ralph Gutkowski"
   description "Login as any user with click of a button."
-  version '0.10.0'
+  version '1.0.0'
   url 'https://github.com/rgtk/redmine_impersonate'
   author_url 'https://github.com/rgtk'
+
+  requires_redmine version_or_higher: '3.0.0'
 end
 
 RedmineImpersonate.install
